d45b49838434f18e260c22c4d0aa8e4084bbb387
abqu <erick_abqu@hotmail.com>
2019-09-15 20:57:55 -0500

    Update grammys.js
    actual json

--- grammys/js/grammys.js
+++ grammys/js/grammys.js
@@ -6,16 +6,26 @@ $.ajax({
 
 		let newHtml = ''
 
-		$(data).find('fields').each(function() {
+		for(let i = 0; i < data.length; i++) {
 			newHtml += `
-				<option value= "${$(this).attr('id')}">
-					${$(this).attr('field')}
+				<option value="${data[i].field_id}">
+					${data[i].field}
 				</option>
 				`
-		})
+		}
 		$('#category_types').append(newHtml)
+		loadDatos()
 	},
 	error : function(errorMsg) {
-		console.log(errorMsg)
+		console.log("NOTLOAD")
 	}
 })
+
+function loadDatos(){
+	url : 'https://erickmedinafleming.github.io/lab3Ajax/grammys/data/grammys.json',
+	type : 'GET',
+	dataType : 'json',
+	success : function(data) {
+		
+	}
+}
