8f2768e89b77142800d2b56c2586d2d9162d8744
abqu <erick_abqu@hotmail.com>
2019-09-15 20:30:56 -0500

    does it
    ajax first attempt to work

--- grammys/js/grammys.js
+++ grammys/js/grammys.js
@@ -1,3 +1,21 @@
 $.ajax({
-	
+	url : 'https://erickmedinafleming.github.io/lab3Ajax/grammys/data/grammys.json',
+	type : 'GET',
+	dataType : 'json',
+	success : function(data) {
+
+		let newHtml = ''
+
+		$(data).find('fields').each(function() {
+			newHtml += `
+				<option value= "${$(this).attr('id')}">
+					${$(this).attr('field')}
+				</option>
+				`
+		})
+		$('#category_types').append(newHtml)
+	},
+	error : function(errorMsg) {
+		console.log(errorMsg)
+	}
 })
